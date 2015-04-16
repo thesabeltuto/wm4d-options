e73a9fb8213544a5f449a15b8fd9dc830e15a66b
Thesabel Tuto <thesabeltuto@gmail.com>
2015-04-16 23:58:09 +0800

    3.1.3 - fix on admin js

--- js/admin.js
+++ js/admin.js
@@ -9,21 +9,17 @@ function multiple_select() {
 	if( jQuery('#wm4d_select_options input').attr('checked') ) {
 		jQuery('div#wm4d_primary_options').hide();
 		jQuery('div#wm4d_multiple_options').show();
-		jQuery('.wm4d_select_primary').hide();
-		jQuery('.wm4d_select_multiple').show();
 	} else {
 		jQuery('div#wm4d_primary_options').show();
 		jQuery('div#wm4d_multiple_options').hide();
+	}
+	
+	if( jQuery('#wm4d_select_options_flipper input').attr('value') == 'primary' ) {
 		jQuery('.wm4d_select_primary').show();
 		jQuery('.wm4d_select_multiple').hide();
 	}
-	
-//	if( jQuery('#wm4d_select_options_flipper input').attr('value') == 'primary' ) {
-//		jQuery('.wm4d_select_primary').show();
-//		jQuery('.wm4d_select_multiple').hide();
-//	}
-//	if( jQuery('#wm4d_select_options_flipper input').attr('value') == 'multiple' ) {
-//		jQuery('.wm4d_select_primary').hide();
-//		jQuery('.wm4d_select_multiple').show();
-//	}
+	if( jQuery('#wm4d_select_options_flipper input').attr('value') == 'multiple' ) {
+		jQuery('.wm4d_select_primary').hide();
+		jQuery('.wm4d_select_multiple').show();
+	}
 }
