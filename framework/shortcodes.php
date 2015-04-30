e87e663932addf69b0efd8844f3d01571582f160
Thesabel Tuto <thesabeltuto@gmail.com>
2015-04-30 23:42:03 +0800

    Version 3.1.4

--- framework/shortcodes.php
+++ framework/shortcodes.php
@@ -26,6 +26,11 @@ if ( get_option('wm4d_multiple_select') == 'enable' ) {
 	add_shortcode( 'locations', 'wm4d_locations' );	
 }
 
+add_shortcode( 'offer', 'special_offer' );
+add_shortcode( 'testimonials', 'testimonials' );
+add_shortcode( 'before_afters', 'before_afters' );
+add_shortcode( 'office_images', 'office_images' );
+
 function wm4d_before_afters( $atts ){
 	$wm4d_before_afters = get_option('wm4d_before_afters');
 	return $wm4d_before_afters;
@@ -163,5 +168,116 @@ function wm4d_locations( $atts ){
 function wm4d_practice( $atts ){
 	$wm4d_practice = get_option('wm4d_practice');
 	return $wm4d_practice;
+}
+
+function special_offer($atts){
+	extract(shortcode_atts(array(
+		'title' => 'Special Offer',
+		'price' => '',
+		'service' => '',
+		'options' => '',
+		'tagline' => '',
+		'consult' => 'Request Appointment',
+		'extend' => 'Extend Offer',
+		'consult_form_id' => '',
+		'extend_form_id' => '',
+		'consult_form_url' => '',
+		'extend_form_url' => ''
+	), $atts ));
+	$offer_expires_date = date("Y-m-d");
+	
+	$special_offer = '<div id-"special-offer">'; 
+	$special_offer .= '<div id="offer-heading">'.$title.'</div>';
+	$special_offer .= '<div id="offer-wrap"><div id="offer"><h2>' . $price . '</h2>';
+	$special_offer .= '<span>' . $service_offer . '</span></div>';
+	$special_offer .= '<div id="offer-details"><div id="offer-features">' . $options . '</div></div>';
+	$special_offer .= '<div id="tagline">'. $tagline . '</div>'; /*OpenInNewTab();*/
+	$special_offer .= '<div id="consult-btn-text"><div class="various" href="#gform-consult-form-'. $consult_form_id . '"><a href="' . $consult_form_url . '" target="_blank">' . $consult. '</a></div></div>';
+	$special_offer .= '<div id="expire-offer">Offer expires: ' . $offer_expires_date . '</div>';
+	$special_offer .= '<div id="extend-offer"><div class="various" href="#gform-extend-form-'. $extend_form_id . '"><a href="' . $extend_form_url . '" target="_blank" >' . $extend . '</a></div></div>';
+	$special_offer .= '<div id="gform-consult-form-' .  $consult_form_id . '" style="display:none;width:500px;">';
+	$special_offer .= do_shortcode('[gravityform id="'. $consult_form_id . '" ajax="true"]');
+	$special_offer .= '</div>';
+	$special_offer .= '<div id="gform-extend-form-' .  $extend_form_id . '" style="display:none;width:500px;">';
+	$special_offer .= do_shortcode('[gravityform id="'. $extend_form_id . '" ajax="true"]');
+	$special_offer .= '</div>';
+	$special_offer .= '</div>';
+	
+	return $special_offer;
+}
+
+function testimonials( $atts ) {
+	extract(shortcode_atts(array(
+		'title' => 'Testimonials',
+		'all' => 'See All',
+		'url' => '/testimonials/'
+	), $atts ));
+	
+	$testimonials = '<div id="testimonials">';
+	$testimonials .= '<h2 class="widget-title">'.$title.'</h2>';
+	$testimonials .= '<div id="cycle" style="height:auto!important;max-height:300px!important;">';
+	
+	$slider_args = array('post_type' => 'testimonials', 'posts_per_page' => -1);
+	$loop = new WP_Query($slider_args);
+	while ($loop->have_posts()) : $loop->the_post();
+		$testimonials .= '<div class="the-testimonial"><div class="testimonial-excerpt">'. get_the_excerpt() . '</div>';
+		$testimonials .= '<div class="testimonial-title"> &mdash; ' . get_the_title() . '</div></div>';
+	endwhile;
+		
+	$testimonials .= '</div>';
+	$testimonials .= '<div id="testimonial-nav">';
+	$testimonials .= '<a href="#"><span id="prev">Prev</span></a>'; 
+	$testimonials .= '<a href="'. $url .'"><span id="seeall">'. $all .'</span></a>'; 
+	$testimonials .= '<a href="#"><span id="next">Next</span></a>';
+	$testimonials .= '</div>';
+	$testimonials .= '</div>';
+	
+	return $testimonials;
+}
+
+function before_afters( $atts ) {
+	extract(shortcode_atts(array( 'title' => 'Before and Afters' ), $atts ));
+	
+	$bna = '<div id="before-after">';
+	$bna .= '<h2 class="widget-title">'.$before_after_title.'</h2>';
+	$bna .= '<ul id="before-after-cycle">';
+	
+	$before_after_args = array('post_type' => 'before-and-afters', 'posts_per_page' => -1);
+	$before_after_loop = new WP_Query($before_after_args);
+	while ($before_after_loop->have_posts()) : $before_after_loop->the_post();
+		$bna .= the_post_thumbnail();
+	endwhile;
+		
+	$bna .= '</ul>';
+	$bna .= '<div id="before-after-nav">';
+	$bna .= '<a href="#"><span id="before-after-prev">Prev</span></a>'; 
+	$bna .=  '<a href="#"><span id="before-after-next">Next</span></a>';
+	$bna .= '</div>';
+	$bna .= '</div>';
+	
+	return $bna;
+}
+
+function office_images( $atts ) {
+	extract(shortcode_atts(array( 'title' => 'Office Images' ), $atts ));
+	
+	$office = '<div id="office-images">';
+	$office .= '<h2 class="widget-title">'.$office_images_title.'</h2>';
+	$office .= '<ul id="office-images-cycle">';
+	
+	$office_images_args = array('post_type' => 'office-images', 'posts_per_page' => -1);
+	$office_images_loop = new WP_Query($office_images_args);
+	while ($office_images_loop->have_posts()) : $office_images_loop->the_post();
+		$office .= the_post_thumbnail();
+	endwhile;
+		
+	$office .= '</ul>';
+	$office .= '<div id="office-images-nav">';
+	$office .= '<a href="#"><span id="office-images-prev">Prev</span></a>'; 
+	$office .=  '<a href="#"><span id="office-images-next">Next</span></a>';
+	$office .= '</div>';
+	$office .= '</div>';
+
+	return $office;
 }
 ?>
