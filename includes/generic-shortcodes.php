5465236e6e39fdfc520f490caf0874f3ea161708
Thesabel Tuto <thesabeltuto@gmail.com>
2017-08-18 05:44:57 +0800

    3.4.1
    3.4.1

--- includes/generic-shortcodes.php
+++ includes/generic-shortcodes.php
@@ -6,12 +6,11 @@
 //add_filter('the_excerpt', 'do_shortcode');
 //add_filter('gform_notification', 'do_shortcode', 10, 3);
 //add_filter('wp_init', 'do_shortcode');
-add_filter('this_year', 'do_shortcode');
-
 add_shortcode( 'this_year', 'get_this_year' );	
 
 function get_this_year( $atts ){
 	$the_year = date("Y");
+	
 	return $the_year;
 }
 
