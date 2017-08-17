d4e5516a6cd13e81d2154fbb61c0773e062afea2
Thesabel Tuto <thesabeltuto@gmail.com>
2017-08-17 22:32:07 +0800

    3.4.1
    3.4.1

--- wm4d-options.php
+++ wm4d-options.php
@@ -5,10 +5,10 @@
 	Description: This plugin is a simplified <a href="http://www.wm4d.com/" target="_blank">WM4D</a> plugin that includes custom post types and widgets of  before and afters, prodecures, offers, office images and testimonials.
 	This plugin also includes theme options that can help you edit styles and scripts on dashboard. Client options has been added to provide flexibilty of information across the website.
 	Number flipper has been added to help you flip phone numbers for specific website visitors.
-	Version: 3.4.0
+	Version: 3.4.1
 	Author: Thesabel Tuto
 	Author URI: http://thesabeltuto.blogspot.com
-	Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=H228JQZP6269J&lc=US&item_name=TT%2dPlugins%3a%20Support%20WordPress%20Plugin%20Development&item_number=TT%2dPlugins&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+	Donate link: https://www.paypal.me/T22Gaming
 	License: GPLv2 or later
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -34,7 +34,7 @@ define('WM4D_OPTIONS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WM4D_OPTIONS_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // Global variables
-$WM4D_OPTIONS_PLUGIN_VERSION = '3.4.0';
+$WM4D_OPTIONS_PLUGIN_VERSION = '3.4.1';
 $WM4D_OPTIONS_PLUGIN_CSS_VERSION = '3.0.9'; // style
 $WM4D_OPTIONS_PLUGIN_JS_VERSION = '3.1.3'; // thescript
 $WM4D_OPTIONS_PLUGIN_JS2_VERSION = '0.1.4'; // phonescript
@@ -127,6 +127,9 @@ function WM4D_OPTIONS_PLUGIN_aftertheme(){
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/framework/shortcodes-inner.php');
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/integrations.php');
 }
+
+//** GENERIC SHORTCODES
+include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/generic-shortcodes.php');
 
 //** MODS PAGE HERE
 include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/mods/settings.php');
