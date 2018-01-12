3ef2b137a9e9242a68dcb30f502b044e69235b7b
Thesabel Tuto <thesabeltuto@gmail.com>
2018-01-12 14:08:25 +0800

    3.4.2 final shit

--- includes/integrations/integrations.php
+++ includes/integrations/integrations.php
@@ -1,7 +1,8 @@
 <?php
 //	general theme settings
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/theme-functions.php');
-		//	soulmedic theme integration settings
+
+	//	soulmedic theme integration settings
 	if( wp_get_theme() == 'soulmedic' ) {
 		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php');
 	}
