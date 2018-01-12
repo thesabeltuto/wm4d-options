f084cdd0f3e43f53a742530e347486764a398186
Thesabel Tuto <thesabeltuto@gmail.com>
2018-01-13 05:53:38 +0800

    3.2.4 integration new

--- includes/integrations/integrations.php
+++ includes/integrations/integrations.php
@@ -3,12 +3,14 @@
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/theme-functions.php');
 	
 //	theme integration settings
-	if( wp_get_theme( 'soulmedic' )->exists() ) {
+	//if( wp_get_theme( 'soulmedic' )->exists() ) {
+	if( wp_get_theme() == 'soulmedic' ) {
+		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
 		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php');
 	}
 
 //	plugin integration settings
-	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
+
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/responsive-map.php');
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/gravityforms.php');
 
