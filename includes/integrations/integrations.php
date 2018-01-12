037ecaa07c9be3f7df90c82fefba7d9eb51fe9e4
Thesabel Tuto <thesabeltuto@gmail.com>
2018-01-12 13:31:31 +0800

    3.4.2 salient integration

--- includes/integrations/integrations.php
+++ includes/integrations/integrations.php
@@ -2,18 +2,14 @@
 //	general theme settings
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/theme-functions.php');
 	
-//	plugin integration settings
+//	soulmedic theme integration settings
 	if( wp_get_theme() == 'soulmedic' ) {
 		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
-	} /*else {
-		add_filter( 'default_hidden_meta_boxes', 'enable_custom_fields_per_default', 20, 1 );	
-	}*/
+		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php');
+	}
+
+//	plugin integration settings
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/responsive-map.php');
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/gravityforms.php');
-	
-//	theme integration settings
-	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php');
-
-
 
 ?>
