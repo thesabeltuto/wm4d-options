37ae1b993755844131340d43553752e91b9fd40e
Thesabel Tuto <thesabeltuto@gmail.com>
2018-01-13 06:10:49 +0800

    3.2.4 fixed integration

--- includes/integrations/integrations.php
+++ includes/integrations/integrations.php
@@ -3,15 +3,14 @@
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/theme-functions.php');
 	
 //	theme integration settings
-	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php');
-	//if( wp_get_theme( 'soulmedic' )->exists() ) {
-	if( wp_get_theme() == 'soulmedic' ) {
-		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
+	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/themes/soulmedic.php'); //soulmedic content tags
+	
+//	theme matches soulmedic name	
+	if( preg_match( '/soulmedic/', wp_get_theme() ) == true )  {
+		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php'); //soulmedic specific metabox
 	}
-
+	
 //	plugin integration settings
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/responsive-map.php');
-	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/gravityforms.php');
-
-//	add_filter( 'default_hidden_meta_boxes', 'enable_custom_fields_per_default', 20, 1 );	
+	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/gravityforms.php');	
 ?>
