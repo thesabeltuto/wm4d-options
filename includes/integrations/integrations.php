32e8a6f00567ba38efa54f1a1fd002a10566d86c
Thesabel Tuto <thesabeltuto@gmail.com>
2018-01-12 14:00:06 +0800

    3.4.2 fix it bitch

--- includes/integrations/integrations.php
+++ includes/integrations/integrations.php
@@ -2,14 +2,6 @@
 //	general theme settings
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/theme-functions.php');
 	
-<<<<<<< HEAD
-//	plugin integration settings
-	if( wp_get_theme( 'soulmedic' )->exists() ) {
-	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
-	} else {
-		add_filter( 'default_hidden_meta_boxes', 'enable_custom_fields_per_default', 20, 1 );	
-	}
-=======
 //	soulmedic theme integration settings
 	if( wp_get_theme() == 'soulmedic' ) {
 		include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/layerslider.php');
@@ -17,7 +9,6 @@
 	}
 
 //	plugin integration settings
->>>>>>> refs/remotes/origin/master
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/responsive-map.php');
 	include(WM4D_OPTIONS_PLUGIN_DIR.'/includes/integrations/plugins/gravityforms.php');
 
